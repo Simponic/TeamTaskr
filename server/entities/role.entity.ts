8a87d4da7992d449e16728f13fe535cb73a4bdbe
Logan Hunt <loganhunt@simponic.xyz>
2022-02-09 15:32:09 -0700

    Remove compare function that never ended up being used

--- server/entities/role.entity.ts
+++ server/entities/role.entity.ts
@@ -12,12 +12,6 @@ export enum RoleKey {
 @Entity()
 export class Role {
   static ROLES = [RoleKey.ADMIN, RoleKey.TEAM_LEADER, RoleKey.TEAM_MEMBER, RoleKey.USER];
-  static compare(role1: RoleKey, role2: RoleKey) {
-    // compare(role1, role2) such that (role1  > role2) => 1
-    //                                 (role1  < role2) => -1
-    //                                 (role1 == role2) => 0
-    return Math.max(-1, Math.min(1, Role.ROLES.indexOf(role2) - Role.ROLES.indexOf(role1)));
-  }
 
   @PrimaryGeneratedColumn()
   id: number;
